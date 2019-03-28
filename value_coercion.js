23ef12c69abefbf0eeb09026e20dd2a510446a43
Dorian Kcira <dkcira@gmail.com>
2019-03-27 21:10:37 -0700

    initial js files

---
+++ value_coercion.js
@@ -0,0 +1,8 @@
+
+// explicit coercion
+var a = "42";
+
+var b = Number(a);
+
+console.log(`${a}`); // string "42"
+console.log(`${b}`); // number 42
---
+++ vars.js
@@ -0,0 +1,16 @@
+
+// use typeof operator to find type of variables / objects
+
+var a; console.log(`${typeof a}`); // undefined
+
+a = "hello world"; console.log(`${typeof a}`); // string
+
+a = 42; console.log(`${typeof a}`); // number
+
+a = true; console.log(`${typeof a}`); // boolean
+
+a = null; console.log(`${typeof a}`); // object? -- weird, bug
+
+a = undefined; console.log(`${typeof a}`); // undefined
+
+a = {b: "c"}; console.log(`${typeof a}`); // object
