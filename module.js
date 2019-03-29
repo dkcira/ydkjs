2bd4134744cc09002e434dbc1a2bf932d58ce38c
Dorian Kcira <dkcira@gmail.com>
2019-03-28 21:28:55 -0700

    module

--- module.js
+++ module.js
@@ -1,3 +1,8 @@
+// The User() function serves as an outer scope that holds the variables
+// username and password, as well as the inner doLogin() function;
+// these are all private inner details of this User module that cannot
+// be accessed from the outside world.
+
 function User(){
 	var username, password;
 
